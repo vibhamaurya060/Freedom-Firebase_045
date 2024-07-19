242f0cdffe312e1de1b6ffe470dc6d4b4039f896
NikitaMane12 <manenikita2212@gmail.com>
2024-07-19 22:22:22 +0530

    home content

--- frontend/src/AllRoutes/Allrouters.jsx
+++ frontend/src/AllRoutes/Allrouters.jsx
@@ -1,24 +1,23 @@
-import { Link } from '@chakra-ui/react'
-import {Routes, Route} from 'react-router-dom'
-import React from 'react'
-import { Find } from '../pages/Find'
-import { Home } from '../pages/Home'
-import { BookEvents } from '../pages/BookEvents'
-import { CreateEvents } from '../pages/CreateEvents'
-import { Login } from '../pages/Login'
-import { SignUp } from '../pages/SignUp'
+import { Routes, Route } from "react-router-dom";
+
+import { Find } from "../pages/Find";
+import { Home } from "../pages/Home";
+import { BookEvents } from "../pages/BookEvents";
+import { CreateEvents } from "../pages/CreateEvents";
+import { Login } from "../pages/Login";
+import { SignUp } from "../pages/Signup";
 
 export const Allrouters = () => {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/find" element={<Find />} />
-                <Route path="/bookevent" element={<BookEvents />} />
-                <Route path="/createEvents" element={<CreateEvents />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp/>} />
-            </Routes>
-        </>
-    )
-}
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/find" element={<Find />} />
+        <Route path="/bookevent" element={<BookEvents />} />
+        <Route path="/createEvents" element={<CreateEvents />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </>
+  );
+};
